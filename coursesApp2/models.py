2b28fe2ef5eb20c3e2a38e7225a52f53af245f9b
castillocodes <janeirosergio@gmail.com>
2021-09-14 10:32:51 -0500

    Delete page

--- coursesApp2/models.py
+++ coursesApp2/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class CourseManager(models.Manager):
+    def create_validator(self, postData):
+        errors = {}
+        if len(postData['course_name']) < 6:
+            errors['name'] = "Course name is too short"
+        if len(postData['description']) < 16:
+            errors['desc'] = "Description is too short"
+        return errors
+
 class Course(models.Model):
     name = models.TextField()
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = CourseManager()
--- coursesApp2/urls.py
+++ coursesApp2/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('courses/create', views.create_course),
+    path('courses/destroy/<int:course_id>', views.destroy_course),
+    path('courses/delete/<int:course_id>', views.delete_course),
 ]
